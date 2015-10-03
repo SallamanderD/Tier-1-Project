fe3f4473f7e8158832b677df5ba8c895ebc3ac3c
Dorotenko Alexandr <Sallamanderdr@gmail.com>
2015-10-03 13:26:07 +0300

    ad

---
+++ Turn1/Turn1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Turn1.MvcApplication" Language="C#" %>
---
+++ Turn1/Turn1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Turn1.Startup))]
+namespace Turn1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
