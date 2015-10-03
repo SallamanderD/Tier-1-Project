dff4929e1cdb0f073c245e2fa72ade89952e0a21
Dorotenko Alexandr <Sallamanderdr@gmail.com>
2015-10-03 14:17:19 +0300

    Test

---
+++ Turn1/CodeFirst/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public byte[] Photo { get; set; }
+
+        // Ссылка на заказы
+        public virtual List<Order> Orders { get; set; }
+    }
+}
